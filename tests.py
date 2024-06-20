e2f81a809622c263d094e0bba16759df5fec03f8
Artem Sokolov <ArtemSokolovQA@yandex.ru>
2024-06-20 12:43:39 +0300

    Добавил праметры по КЭ и ГЗ в параметризированный тест

--- tests.py
+++ tests.py
@@ -2,12 +2,6 @@ import pytest
 from main import BooksCollector
 
 
-@pytest.fixture()
-def set_collector():
-    collector = BooksCollector()
-    return collector
-
-
 class TestBooksCollector:
 
     def test_add_new_book_add_two_books(self, set_collector):
@@ -31,7 +25,6 @@ class TestBooksCollector:
     def test_get_books_with_specific_genre(self, set_collector):
         collector = set_collector
         collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
         collector.set_book_genre('Гордость и предубеждение и зомби', 'Фантастика')
         assert collector.get_books_with_specific_genre('Фантастика') == ['Гордость и предубеждение и зомби']
 
@@ -71,10 +64,13 @@ class TestBooksCollector:
         assert collector.get_list_of_favorites_books() == ['Гордость и предубеждение и зомби', 'Что делать, если ваш кот хочет вас убить']
 
     @pytest.mark.parametrize('book_name', [
-        "Книга с допустимой длиной",
+        "А",
         "Война и Мир",
-        "Убить пересмешника",
-        "The catcher in the rye"
+        "Lorem ipsum dolor sit amet, consectetuer",
+        "Lorem ipsum dolor sit amet, consectetue",
+        "复写 复制 临摹 描摹 抄袭 仿效 模仿 印相 制拷贝",
+        "!№;?*();?:%№%:,.?*",
+        "          "
     ])
     def test_add_new_book_parametrized(self, set_collector, book_name):
         collector = set_collector
